048e14a03d91d0c644080bccf2701d893e68be20
Jonnie <jonniediegelman@gmail.com>
2020-12-02 09:31:34 -0500

    You'd better watch out, you'd better not cry

--- scripts/Day2/code.jl
+++ scripts/Day2/code.jl
@@ -3,25 +3,43 @@ module Day2
 ## Setup
 using Advent2020
 
+function parse_line(str)
+    lohi, letter, password = split(str, " ")
+    lo, hi = parse.(Int, split(lohi, "-"))
+    letter = string(letter[1])
+    return (; lo, hi, letter, password)
+end
+
+read_input(io) = parse_line.(readlines(io))
+
+valid_password1(line) = line.lo ≤ count(string(line.letter), line.password) ≤ line.hi
+
+valid_password2(line) =
+    string(line.password[line.lo])==line.letter && !(string(line.password[line.hi])==line.letter) ||
+    !(string(line.password[line.lo])==line.letter) && string(line.password[line.hi])==line.letter
 
 ## Inputs (change these)
-const test_input = nothing
-const test_output1 = nothing
-const test_output2 = nothing
+const test_input = read_input(
+    IOBuffer(
+        """
+        1-3 a: abcde
+        1-3 b: cdefg
+        2-9 c: ccccccccc
+        """
+    )
+)
+const test_output1 = 2
+const test_output2 = 1
 
-const data = nothing
+const data = read_input(joinpath(@__DIR__, "input.txt"))
 
 
 ## Solution functions (change these)
 # Part 1
-function get_solution1(data)
-    return nothing
-end
+get_solution1(data) = sum(valid_password1.(data))
 
 # Part 2
-function get_solution2(data)
-    return nothing
-end
+get_solution2(data) = sum(valid_password2.(data))
 
 
 ## Outputs (don't change these)
--- src/common.jl
+++ src/common.jl
@@ -1,6 +1,6 @@
 module Common
 
-export zero_based
+export zero_based, run
 
 
 """
@@ -9,5 +9,12 @@ export zero_based
 Make an array that indexes from 0 instead of 1.
 """
 zero_based(array) = OffsetArray(array, -1)
+
+"""
+    run(mod)
+
+Run a module `mod`.
+"""
+run(mod) = mod.run()
 
 end
