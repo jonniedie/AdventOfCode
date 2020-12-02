5c58da60e028b74c93738f5a2af6e4ab851b2d27
Jonnie Diegelman <jonathan.diegelman@nasa.gov>
2020-12-02 10:46:05 -0500

    We're singing deck the halls

--- template/code.jl
+++ template/code.jl
@@ -1,11 +1,11 @@
 module DayN
 
 ## Setup
-using Advent2020
 
 
 ## Inputs (change these)
-const test_input = nothing
+const test_input1 = nothing
+const test_input2 = nothing
 const test_output1 = nothing
 const test_output2 = nothing
 
@@ -23,20 +23,5 @@ function get_solution2(data)
     return nothing
 end
 
-
-## Outputs (don't change these)
-function run(time=true)
-    # Do not pass if tests don't check out
-    @assert get_solution1(test_input) == test_output1
-    @assert get_solution2(test_input) == test_output2
-
-    println("Solution 1: $(get_solution1(data))")
-    time && @time get_solution1(data)
-
-    println("Solution 2: $(get_solution2(data))")
-    time && @time get_solution2(data)
-
-    return nothing
-end
 
 end
