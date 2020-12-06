e60a0c0bb1caf93f134431550ffb9f3959d8eba0
Jonnie <jonniediegelman@gmail.com>
2020-12-06 01:04:49 -0500

    Oh Holy night, the stars are brightly shiiiiiining

---
+++ scripts/Day6/code.jl
@@ -0,0 +1,26 @@
+module Day6
+export get_inputs, get_solution1, get_solution2
+
+
+## Input getting
+parse_declaration(str) = filter(!isempty, Set(collect.(split(str, r"\n|\r\n"))))
+
+read_inputs(f_name) = parse_declaration.(split(read(joinpath(@__DIR__, f_name), String), r"\n\n|\r\n\r\n"))
+
+function get_inputs()
+    test_input1 = test_input2 = read_inputs("test_input1.txt")
+    test_output1 = 11
+    test_output2 = 6
+    data = read_inputs("input.txt")
+    return (; test_input1, test_input2, test_output1, test_output2, data)
+end
+
+
+## Solution functions
+# Part 1
+get_solution1(data) = sum(length.(reduce.(union, data)))
+
+# Part 2
+get_solution2(data) = sum(length.(reduce.(intersect, data)))
+
+end
