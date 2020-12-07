1b0cae6314e9efe0e0d31b6688d66e3aec75b001
Jonnie Diegelman <jonathan.diegelman@nasa.gov>
2020-12-07 10:20:41 -0500

    He wore my coat on Christmas morn

--- scripts/Day7/code.jl
+++ scripts/Day7/code.jl
@@ -6,12 +6,14 @@ using ConcreteStructs, Memoize
 
 
 ## Input getting
+read_inputs(f_name) = make_the_rules(readlines(joinpath(@__DIR__, f_name)))
+
 function get_inputs()
-    test_input1 = readlines(joinpath(@__DIR__, "test_input1.txt"))
-    test_input2 = readlines(joinpath(@__DIR__, "test_input2.txt"))
+    test_input1 = read_inputs("test_input1.txt")
+    test_input2 = read_inputs("test_input2.txt")
     test_output1 = 4
     test_output2 = 126
-    data = readlines(joinpath(@__DIR__, "input.txt"))
+    data = read_inputs("input.txt")
     return (; test_input1, test_input2, test_output1, test_output2, data)
 end
 
@@ -20,16 +22,17 @@ end
     num
     desc
 end
+
+const NO_OTHER = Statement(0, "no other")
+
 function Statement(str)
     if str=="no other"
-        return Statement(0, "")
+        return NO_OTHER
     else
         words = split(str, " ")
         return Statement(parse(Int, words[1]), join(words[2:end], " "))
     end
 end
-
-const NO_OTHER = Statement(0, "")
 
 num_bags(statement) = statement.num
 description(statement) = statement.desc
@@ -39,7 +42,7 @@ description(statement) = statement.desc
 # Parse a single line of the rules
 function parse_rule(line)
     line = split(line, r" bags contain | bag contains |, | bags| bag", keepempty=false)
-    return String(line[1])=>Statement.(line[2:end-1])
+    return String(line[1]) => Statement.(line[2:end-1])
 end
 
 # Make the rule dict
@@ -48,38 +51,31 @@ make_the_rules(lines) = Dict(parse_rule.(lines))
 
 ## Solution functions
 # Part 1
-@memoize function contains_bag(dict, outer, inner)
-    statements = dict[outer]
+@memoize function contains_bag(rules, outer, inner)
+    statements = rules[outer]
     if statements[1]==NO_OTHER
         return false
     elseif inner in description.(statements)
         return true
     else
-        return any(contains_bag(dict, description(statement), inner) for statement in statements)
+        return any(contains_bag(rules, description(statement), inner) for statement in statements)
     end
 end
 
-function get_solution1(data; bag="shiny gold")
-    rules = make_the_rules(data)
-    return sum(contains_bag(rules, head, bag) for head in keys(rules))
-end
+get_solution1(rules; bag="shiny gold") = sum(contains_bag(rules, head, bag) for head in keys(rules))
 
 
 # Part 2
-@memoize function count_bags(dict, bag)
-    statements = dict[bag]
+@memoize function count_bags(rules, bag)
+    statements = rules[bag]
     if statements[1]==NO_OTHER
         return 0
     else
         top_level = sum(num_bags.(statements))
-        return top_level + sum(num_bags(st) * count_bags(dict, description(st)) for st in statements)
+        return top_level + sum(num_bags(st) * count_bags(rules, description(st)) for st in statements)
     end
 end
 
-
-function get_solution2(data; bag="shiny gold")
-    rules = make_the_rules(data)
-    return count_bags(rules, bag)
-end
+get_solution2(rules; bag="shiny gold") = count_bags(rules, bag)
 
 end
