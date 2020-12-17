1b92148fdc694fe176a85d08a5702fee72c0ee47
Jonnie Diegelman <jonathan.diegelman@nasa.gov>
2020-12-17 11:44:13 -0500

    in a one-horse open sleigh

--- scripts/Day17/code.jl
+++ scripts/Day17/code.jl
@@ -2,8 +2,7 @@ module Day17
 
 export get_inputs, get_solution1, get_solution2
 
-using DataStructures: DefaultDict
-using OffsetArrays: OffsetArray
+using OffsetArrays
 
 
 ## Input getting
@@ -29,37 +28,43 @@ end
 
 
 ## Solution functions
-function make_space(active_matrix, dims=3)
+# Allocate an OffsetArray for the space the active cells will grow into
+function make_space(active_matrix, dims=3, steps=6)
     init_dim_size = (axes(active_matrix)..., ntuple(d->0, dims-2)...)
-    dim_size = reduce((dims, _) -> grow(dims), 1:7, init=init_dim_size)
-    space = OffsetArray(zeros(Bool, dim_size...), dim_size...)
+    dim_size = reduce((dims, _) -> grow(dims), 1:steps+1, init=init_dim_size)
+
+    # This will make an OffsetArray with indices that reach back into the negatives
+    space = zeros(Bool, dim_size...)
     for ij in CartesianIndices(active_matrix)
         space[Tuple(ij)..., ntuple(d->0, dims-2)...] = active_matrix[ij]
     end
     return space
 end
 
+# Adjacent indices of (0,0,0,...)
 adj_inds(dims) = (
     Tuple(ijk)
     for ijk in CartesianIndices(ntuple(i->-1:1, dims))
     if !all(Tuple(ijk) .== 0)
 )
+# Get the adjacent cells
+get_adjacent(data, idx) = (data[(adj_idx .+ idx)...] for adj_idx in adj_inds(ndims(data)))
+get_adjacent(data, idx::CartesianIndex) = get_adjacent(data, idx.I)
 
-get_adjacent(data, idx_tuple) = (data[(i .+ idx_tuple)...] for i in adj_inds(ndims(data)))
-
+# Step the simulation by iterating through the bounding_box axes
 function step!(space, temp, bounding_box)
-    for ijk in CartesianIndices(bounding_box)
-        adjacents = get_adjacent(space, Tuple(ijk))
-        count_adj = count(adjacents)
-        if space[ijk] == true
-            temp[ijk] = count_adj==2 || count_adj==3
+    for idx in CartesianIndices(bounding_box)
+        count_adj = count(get_adjacent(space, idx))
+        temp[idx] = if space[idx] == true
+            count_adj==2 || count_adj==3
         else
-            temp[ijk] = count_adj==3
+            count_adj==3
         end
     end
     space .= temp
 end
 
+# Grow a bounding box of array axes
 grow(box) = map(range -> first(range)-1:last(range)+1, box)
 
 
@@ -72,7 +77,7 @@ function get_solution1(data, dims=3)
         step!(space, temp, bounding_box)
         bounding_box = grow(bounding_box)
     end
-    return count(values(space))
+    return count(space)
 end
 
 # Part 2
