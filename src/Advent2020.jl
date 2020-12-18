6ed32e050bb8efe055fb89044f8188c49272d54c
Jonnie <jonniediegelman@gmail.com>
2020-12-18 09:49:22 -0500

    Here comes Santa Claus here comes Santa Claus

--- src/Advent2020.jl
+++ src/Advent2020.jl
@@ -10,7 +10,7 @@ include(joinpath("EventUtils", "EventUtils.jl"))
 
 @reexport using .EventUtils
 
-for i in 1:17
+for i in 1:18
     dir = joinpath(@__DIR__, "..", "scripts", "Day"*string(i))
     if isdir(dir)
         include(joinpath(dir, "code.jl"))
--- template/code.jl
+++ template/code.jl
@@ -6,8 +6,8 @@ export get_inputs, get_solution1, get_solution2
 ## Input getting
 function get_inputs()
     test_input1 = nothing
-    test_input2 = nothing
     test_output1 = nothing
+    test_input2 = nothing
     test_output2 = nothing
     data = readlines(joinpath(@__DIR__, "input.txt"))
     return (; test_input1, test_input2, test_output1, test_output2, data)
