d111b189a46db75327708b81361f4c0064bd65f8
Jonnie Diegelman <jonathan.diegelman@nasa.gov>
2020-12-22 15:50:15 -0500

    ooooooaaaaah Merry  Christmas Saint Nick

--- scripts/Day22/code.jl
+++ scripts/Day22/code.jl
@@ -2,8 +2,6 @@ module Day22
 
 export get_inputs, get_solution1, get_solution2
 
-# using DataStructures: Deque
-
 
 ## Input getting
 function get_inputs()
@@ -22,43 +20,19 @@ end
 
 
 ## Solution functions
-get_score(winning_deck) = winning_deck' * reverse(only(axes(winning_deck)))
-
-get_solution(game, decks) = get_score(game(deepcopy(decks)...).deck)
-
-
-# Part 1
-function combat(deck1, deck2)
-    winning_deck = deck1
-
-    while true
-        card1 = popfirst!(deck1)
-        card2 = popfirst!(deck2)
-
-        card1 > card2 ? push!(deck1, card1, card2) : push!(deck2, card2, card1)
-
-        isempty(deck1) && return (deck=deck2, id=2)
-        isempty(deck2) && return (deck=deck1, id=1)
-    end
-end
-
-get_solution1(data) = get_solution(combat, data)
-
-# Part 2
-function recursive_combat(deck1, deck2)
-    winning_deck = deck1
-
+function combat(deck1, deck2, recursive=false)
     previous_rounds = Vector{Int}[]
     while true
-        deck1 in previous_rounds && return (; deck=winning_deck, id=1)
-        
-        push!(previous_rounds, copy(deck1))
+        if recursive
+            deck1 in previous_rounds && return (; deck=deck1, id=1)
+            push!(previous_rounds, copy(deck1))
+        end
 
         card1 = popfirst!(deck1)
         card2 = popfirst!(deck2)
 
-        if card1<=length(deck1) && card2<=length(deck2)
-            out = recursive_combat(deck1[1:card1], deck2[1:card2])
+        if recursive && card1<=length(deck1) && card2<=length(deck2)
+            out = combat(deck1[1:card1], deck2[1:card2], recursive)
             out.id==1 ? push!(deck1, card1, card2) : push!(deck2, card2, card1)
         elseif card1 > card2
             push!(deck1, card1, card2)
@@ -71,5 +45,15 @@ function recursive_combat(deck1, deck2)
     end
 end
 
-get_solution2(data) = get_solution(recursive_combat, data)
+get_score(winning_deck) = winning_deck' * reverse(only(axes(winning_deck)))
+
+get_solution(decks, recursive=false) = get_score(combat(deepcopy(decks)..., recursive).deck)
+
+
+# Part 1
+get_solution1(data) = get_solution(data)
+
+# Part 2
+get_solution2(data) = get_solution(data, true)
+
 end
