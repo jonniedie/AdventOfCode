fcf1883065054315853af827a351f4e3a23caf8e
Jonnie Diegelman <jonniediegelman@gmail.com>
2022-12-04 11:46:45 -0500

    .

--- years/Advent2022/days/Day4.jl
+++ years/Advent2022/days/Day4.jl
@@ -28,14 +28,12 @@ end
 
 
 ## Solution functions
-function fully_contains(sections)
-    (a1, b1), (a2, b2) = extrema.(sections)
-    return (a1<=a2 && b1>=b2) || (a2<=a1 && b2>=b1)
+function fully_contains((s1, s2))
+    return (s1[1]<=s2[1] && s1[2]>=s2[2]) || (s2[1]<=s1[1] && s2[2]>=s1[2])
 end
 
-function overlaps(sections)
-    (a1, b1), (a2, b2) = extrema.(sections)
-    return (b1>=a2 && a1<=a2) || (b2>=a1 && a2<=a1)
+function overlaps((s1, s2))
+    return (s1[2]>=s2[1] && s1[1]<=s2[1]) || (s2[2]>=s1[1] && s2[1]<=s1[1])
 end
 
 # Part 1
