4d118da333a925eaad5fde0d92741f1d11bc8d07
Jonnie Diegelman <jonniediegelman@gmail.com>
2022-12-04 08:52:01 -0500

    🪞😱 ahhhhhhh

--- years/Advent2022/days/Day4.jl
+++ years/Advent2022/days/Day4.jl
@@ -5,25 +5,42 @@ using ..Advent2022: split_string_lines, read_input
 export get_inputs, get_solution1, get_solution2
 
 ## Input getting
+parse_sections(str) = range(parse.(Int, split(str, '-'))...)
+
+parse_line(str) = Tuple(parse_sections.(split(str, ',')))
+
 function get_inputs()
-    test_input1 = nothing
-    test_output1 = nothing
-    test_input2 = nothing
-    test_output2 = nothing
-    data = read_input(4)
+    test_input1 = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        """ |> split_string_lines .|> parse_line
+    test_output1 = 2
+    test_input2 = test_input1
+    test_output2 = 4
+    data = read_input(4) |> split_string_lines .|> parse_line
     return (; test_input1, test_input2, test_output1, test_output2, data)
 end
 
 
 ## Solution functions
-# Part 1
-function get_solution1(data)
-    return nothing
+function fully_contains(sections)
+    (a1, b1), (a2, b2) = extrema.(sections)
+    return (a1<=a2 && b1>=b2) || (a2<=a1 && b2>=b1)
 end
 
-# Part 2
-function get_solution2(data)
-    return nothing
+function overlaps(sections)
+    (a1, b1), (a2, b2) = extrema.(sections)
+    return (b1>=a2 && a1<=a2) || (b2>=a1 && a2<=a1)
 end
+
+# Part 1
+get_solution1(data) = count(fully_contains, data)
+
+# Part 2
+get_solution2(data) = count(overlaps, data)
 
 end
